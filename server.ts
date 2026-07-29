b8c6a6d2b532d83c202b6ffe9fb84732774177b9
amari-stack <amari@gigsta.ai>
2026-07-29 18:26:03 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -334,8 +334,7 @@ async function startApp() {
     });
   });
 
-  const PORT = Number(process.env.PORT) || 10000;
-
+ const PORT = Number(process.env.PORT) || 10000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
